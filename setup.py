7bc3b5e6c6d23b6695c432892083ea914fd321b2
Jure Majnik <majnik.jure@gmail.com>
2024-02-12 18:05:12 +0100

    updated setup for pip install (include readme etc)

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import find_packages, setup
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='track2p',
-    packages=['track2p']
-)
+    version='2.0.1',
+    packages=['track2p'],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scikit-image',
+        'itk-elastix'],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    )
