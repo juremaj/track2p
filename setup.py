f71fba43facaa1669a7c50f417ef46e6d0cbce06
Jure Majnik <majnik.jure@gmail.com>
2024-03-22 15:10:53 +0100

    added raster plotting dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='track2p',
-    version='0.4.4',
+    version='0.5.0',
     packages=find_packages(),
     install_requires=[
         'numpy==1.23.5',
@@ -14,6 +14,9 @@ setup(
         'itk-elastix==0.19.1',
         'PyQt5==5.15.10',
         'qtpy==2.4.1',
+        'tqdm==4.66.2',
+        'scikit-learn==1.4.0',
+        'openTSNE==1.0.1'
         ],
     long_description=long_description,
     long_description_content_type='text/markdown'
