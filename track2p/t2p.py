13a0878697e5a948f25971c30adbe7052c642268
ManonM30 <manon.mantez@etu.univ-amu.fr>
2024-04-17 16:42:28 +0200

    last test windows

--- track2p/t2p.py
+++ track2p/t2p.py
@@ -31,15 +31,21 @@ def run_t2p(track_ops):
 
     # 4) do the actual registration based on chosen channel
     all_ds_ref_img, all_ds_mov_img = get_all_ds_img_for_reg(all_ds_avg_ch1, all_ds_avg_ch2, track_ops)
+    print("get_all_ds_img_for_reg done")
     all_ds_mov_img_reg, all_ds_reg_params = run_reg_loop(all_ds_ref_img, all_ds_mov_img, track_ops) # TODO: save basic parameters for each registration as feedback (e. g. ammoung of shift, rotation, etc.) for later plotting
+    print("run_reg_loop done")
     plot_reg_img_output(track_ops)
+    print("plot_reg_img_output done")
 
     # 5) apply computed transorm to all ROIs
     all_ds_all_roi_ref, all_ds_all_roi_mov, all_ds_all_roi_reg, all_ds_roi_counter = reg_all_ds_all_roi(all_ds_reg_params, track_ops)
+    print("reg_all_ds_all_roi done")
 
     # 6) optional: generate 'yellow intersection' plot (this is only needed for plotting below)
     all_ds_ref_reg_inters = get_all_ref_nonref_inters(all_ds_all_roi_ref, all_ds_all_roi_reg, track_ops)
+    print("all_ds_ref_reg_inters done")
     all_ds_ref_mov_inters = get_all_ref_nonref_inters(all_ds_all_roi_ref, all_ds_all_roi_mov, track_ops)
+    print("all_ds_ref_mov_inters done")
 
     track_ops.all_ds_ref_mov_inters = all_ds_ref_mov_inters
     track_ops.all_ds_ref_reg_inters = all_ds_ref_reg_inters
@@ -47,17 +53,24 @@ def run_t2p(track_ops):
     # this line is very memory-intensive because of the ROIS (TODO: maybe instead of contours just plot RGB) (or somehow generate RGB image of contours (in the part before))
     if track_ops.show_roi_reg_output:
         plot_roi_reg_output(track_ops)
+        print("plot_roi_reg_output done")
 
     # 7) get optimal assignments for all pairs of recordings (first to last)
     all_ds_assign, all_ds_assign_thr, all_ds_thr_met, all_ds_thr = get_all_ds_assign(track_ops, all_ds_all_roi_ref, all_ds_all_roi_reg)
+    print("get_all_ds_assign done")
     plot_thr_met_hist(all_ds_thr_met, all_ds_thr, track_ops)
+    print("plot_thr_met_hist done")
 
     # 8) get match matrices for all pairs of recordings (first to last)
     all_pl_match_mat = get_all_pl_match_mat(all_ds_all_roi_ref, all_ds_assign_thr, track_ops)
+    print("get_all_pl_match_mat done")
 
     # 9) save results
     save_track_ops(track_ops)
+    print("save_track_ops done")
+    
     save_all_pl_match_mat(all_pl_match_mat, track_ops)
+    print("save_all_pl_match_mat done")
 
     # 10) save in suite2p format
     if track_ops.save_in_s2p_format:
